3c1e74ecc484c89e8279f4f3455e57a2fbf6df40
thomaskeijner <tckeijner@gmail.com>
2025-02-10 20:48:03 +0100

    fix Login css

--- src/Account/Login/Login.tsx
+++ src/Account/Login/Login.tsx
@@ -22,28 +22,26 @@ function Login() {
 
   return (
     <Container className={"text-center"}>
-      <Row className={"justify-content-center"}>
-        <Form onSubmit={handleSubmit} validated={validated}>
-          <div className={"m-2"}>
-            <Col md={6}>
+      <Row className={"justify-content-center m-2"}>
+        <Col md={6}>
+          <Form onSubmit={handleSubmit} validated={validated}>
+            <div className={"m-2"}>
               <Form.Group>
                 <Form.Control type={"text"} id={"username"} placeholder={"Username"} required></Form.Control>
                 <Form.Control.Feedback type="invalid">Username is required</Form.Control.Feedback>
               </Form.Group>
-            </Col>
-          </div>
-          <div className={"m-2"}>
-            <Col md={6}>
+            </div>
+            <div className={"m-2"}>
               <Form.Group>
                 <Form.Control type={"password"} id={"password"} placeholder={"Password"} required></Form.Control>
                 <Form.Control.Feedback type="invalid">Password is required</Form.Control.Feedback>
               </Form.Group>
-            </Col>
-          </div>
-          <div className={"m-2"}>
-            <Button variant={"primary"} className={"w-100"} type={"submit"}>Login</Button>
-          </div>
-        </Form>
+            </div>
+            <div className={"m-2"}>
+              <Button variant={"primary"} className={"w-100"} type={"submit"}>Login</Button>
+            </div>
+          </Form>
+        </Col>
       </Row>
     </Container>
   );
--- src/Welcome/Welcome.tsx
+++ src/Welcome/Welcome.tsx
@@ -1,31 +1,31 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import {Button, Col, Container, Row} from "react-bootstrap";
+import {Link} from "react-router-dom";
 
 function Welcome() {
-    return (
-        <Container className={'text-center'}>
-            <h1 className="h1">Welcome to Flash Cards!</h1>
-            <div>Please log in or create an account</div>
-            <Row className={'justify-content-center m-2'}>
-                <Col className="col-6 col-sm-4 col-md-3">
-                    <Link to={"/login"}>
-                        <Button variant={"primary"} type="button" className="btn btn-primary w-100">
-                            Login
-                        </Button>
-                    </Link>
-                </Col>
-            </Row>
-            <Row className={'justify-content-center m-2'}>
-                <div className="col-6 col-sm-4 col-md-3">
-                    <Link to={"/create-account"}>
-                        <Button variant={"primary"} type="button" className="btn btn-primary w-100">
-                            Create account
-                        </Button>
-                    </Link>
-                </div>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container className={"text-center"}>
+      <h1 className="h1">Welcome to Flash Cards!</h1>
+      <div>Please log in or create an account</div>
+      <Row className={"justify-content-center m-2"}>
+        <Col className="col-6 col-sm-4 col-md-3">
+          <Link to={"/login"}>
+            <Button variant={"primary"} type="button" className="btn btn-primary w-100">
+              Login
+            </Button>
+          </Link>
+        </Col>
+      </Row>
+      <Row className={"justify-content-center m-2"}>
+        <div className="col-6 col-sm-4 col-md-3">
+          <Link to={"/create-account"}>
+            <Button variant={"primary"} type="button" className="btn btn-primary w-100">
+              Create account
+            </Button>
+          </Link>
+        </div>
+      </Row>
+    </Container>
+  );
 }
 
 export default Welcome;
